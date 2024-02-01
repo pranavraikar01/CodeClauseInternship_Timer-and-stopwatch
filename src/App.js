2b8a4d37b00af30e0aeaaeb9a9e54ab636af0d6a
PRANAV RAIKAR <pranavraikar01@gmail.com>
2024-02-01 11:20:39 +0530

    for google analytics

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import "./components/ClockStyles.css";
 function App() {
   return (
     <>
-      <h1>Timmer and Stopwatch</h1>
+      <h1>Timer and Stopwatch</h1>
       <div className="clock-container">
         <Timer />
         <Stopwatch />
@@ -17,18 +17,3 @@ function App() {
 }
 
 export default App;
-// import React from "react";
-// import Timer from "./components/Timer"; // Make sure to provide the correct path
-// import Stopwatch from "./components/Stopwatch"; // Make sure to provide the correct path
-// import "./App.css";
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <Timer />
-//       <Stopwatch />
-//     </div>
-//   );
-// }
-
-// export default App;
